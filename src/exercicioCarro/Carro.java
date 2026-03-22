00444e289fe23913245ec0b8af77c6bfaaf4fb2d
coimbrawanessa <cn.wanessa@outlook.com>
2026-03-21 23:43:12 -0300

    movido pasta exercicioCarro

--- src/exercicioCarro/Carro.java
+++ src/exercicioCarro/Carro.java
@@ -20,4 +20,42 @@ public class Carro {
             velocidade -= 10;
         }
     }
+
+    public static class Carro {
+        public String marca;
+        public int velocidade;
+
+        public void acelerar(){
+            velocidade += 10;
+        }
+
+        public void freiar(){
+            if ( velocidade - 10 < 0){
+                velocidade = 0;
+            }
+            else {
+                velocidade -= 10;
+            }
+        }
+    }
+
+    public static class Main {
+        public static void main(String[] args) {
+            Carro c1 = new Carro();
+            c1.velocidade = 0;
+            for (int i = 0; i < 5;i++ ){
+                c1.acelerar();
+                System.out.println( "velocidade:" + c1.velocidade);
+            }
+
+
+            for ( int i = 0; i < 5; i++){
+                c1.freiar();
+                System.out.println( "velocidade:" + c1.velocidade);
+            }
+
+
+
+        }
+    }
 }
